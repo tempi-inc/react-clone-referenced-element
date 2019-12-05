10a8324943ec3b87bdb63dad9f7d36b8e7b89930
Christopher Dro <hello@blick-labs.com>
2019-12-04 17:09:56 -0800

    Transpile for IE 11

--- cloneReferencedElement.js
+++ cloneReferencedElement.js
@@ -1,32 +1,39 @@
 'use strict';
 
-const React = require('react');
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
 
-function cloneReferencedElement(element, config, ...children) {
-  let cloneRef = config.ref;
-  let originalRef = element.ref;
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var React = require('react');
+
+function cloneReferencedElement(element, config) {
+  var cloneRef = config.ref;
+  var originalRef = element.ref;
+
+  for (var _len = arguments.length, children = new Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
+    children[_key - 2] = arguments[_key];
+  }
+
   if (originalRef == null || cloneRef == null) {
-    return React.cloneElement(element, config, ...children);
+    return React.cloneElement.apply(React, [element, config].concat(children));
   }
 
   if (typeof originalRef !== 'function') {
     if (__DEV__) {
-      console.warn(
-        'Cloning an element with a ref that will be overwritten because it ' +
-        'is not a function. Use a composable callback-style ref instead. ' +
-        'Ignoring ref: ' + originalRef,
-      );
+      console.warn('Cloning an element with a ref that will be overwritten because it ' + 'is not a function. Use a composable callback-style ref instead. ' + 'Ignoring ref: ' + originalRef);
     }
-    return React.cloneElement(element, config, ...children);
+
+    return React.cloneElement.apply(React, [element, config].concat(children));
   }
 
-  return React.cloneElement(element, {
-    ...config,
-    ref(component) {
+  return React.cloneElement.apply(React, [element, _objectSpread({}, config, {
+    ref: function ref(component) {
       cloneRef(component);
       originalRef(component);
-    },
-  }, ...children);
+    }
+  })].concat(children));
 }
 
 module.exports = cloneReferencedElement;
